9f7eb0eaf48c901e26aeda47c46ec28ad8ba9e27
Iván Morett <luisivanmorett@gmail.com>
2018-02-05 17:01:11 -0600

    I changed the framework
    From bootstrap to Foundation

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,3 @@
-$('[data-toggle="popover"]').popover({
-  trigger: "manual",
-}).on("mouseenter", function() {
-  var _this = this;
-  $(this).popover("show");
-  $(".popover").on("mouseleave", function() {
-    $(_this).popover('hide');
-  });
-}).on("mouseleave", function() {
-  var _this = this;
-  setTimeout(function() {
-    if (!$(".popover:hover").length) {
-      $(_this).popover("hide")
-    }
-  }, 100);
-});
-
 $("#menu-bar ul li a[href^='#']").on('click', function(e) {
 
   // prevent default anchor click behavior
